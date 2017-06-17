afdd855b7377b14658d39414d001add0a7683cc9
socool728 <rabihsadi43@gmail.com>
2017-06-17 17:43:41 +0800

    added BiddingService impl

--- opendsp-web/src/main/java/mobi/opendsp/web/CampaignController.java
+++ opendsp-web/src/main/java/mobi/opendsp/web/CampaignController.java
@@ -3,7 +3,16 @@
  */
 package mobi.opendsp.web;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import mobi.opendsp.model.Campaign;
 
 /**
  * @author wangwp
@@ -12,4 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CampaignController {
 
+	@RequestMapping("/campaigns")
+	public List<Campaign> list(@RequestParam(value = "offset") int offset, @RequestParam("limit") int limit) {
+		return null;
+	}
+
+	@RequestMapping("/campaign/{id}")
+	public Campaign load(@PathVariable("id") int campaignId) {
+		return null;
+	}
+
+	@RequestMapping(value = "/campaign/{id}", method = RequestMethod.PUT)
+	public void update(@PathVariable("id") int campaignId, @RequestBody Campaign campaign) {
+	}
 }
